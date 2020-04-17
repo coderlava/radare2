96f01e7c7def5ec1b0137eccb7ce4ce48cda7594
pancake <pancake@nopcode.org>
2020-04-17 11:10:55 +0200

    DISASM is now CODE

--- test/unit/test_arch.c
+++ test/unit/test_arch.c
@@ -14,13 +14,13 @@ bool test_arch_bf(void) {
 	r_arch_instruction_init (&ins);
 
 	r_strbuf_setf (&ins.code, "nop");
-	res = r_arch_encode (a, &ins, R_ARCH_OPTION_DISASM);
+	res = r_arch_encode (a, &ins, R_ARCH_OPTION_CODE);
 	mu_assert ("r_arch_encode", res);
 	mu_assert_eq (ins.size, 1, "encoded instruction size");
 	mu_assert_eq (r_strbuf_get (&ins.data)[0], 0x90, "encoded instruction size");
 
 	r_strbuf_setbin (&ins.data, (const ut8*)"\x2b", 1);
-	res = r_arch_decode (a, &ins, R_ARCH_OPTION_DISASM);
+	res = r_arch_decode (a, &ins, R_ARCH_OPTION_CODE);
 	mu_assert ("r_arch_decode", res);
 
 	const char *opstr = r_strbuf_get (&ins.code);
